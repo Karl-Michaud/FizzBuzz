af666b96008415820d425f43b839177ef7b457d0
KarloSins <92882549+KarloSins@users.noreply.github.com>
2024-09-09 15:57:40 -0400

    New Main file to call the Fizzbuz class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.main(args);
+//        FizzBuzz.main(); fixes proposed are bad because
+        // it either removes the arguments of FizzBuzz.main
+        //making it a normal method or it removes the body of
+        //main.
+    }
 }
